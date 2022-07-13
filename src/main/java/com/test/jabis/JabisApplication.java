9bbe59030c114e0559f95bb35116c74a702d15d0
AE210609 <hhhhhongse@etoos.com>
2022-07-13 12:47:12 +0900

    initial commit

---
+++ src/main/java/com/test/jabis/JabisApplication.java
@@ -0,0 +1,13 @@
+package com.test.jabis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JabisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JabisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/jabis/JabisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.jabis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JabisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
